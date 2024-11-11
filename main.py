7da096b9a6af2e4aa4b292d2dedd6555cb76b899
shawaeon <jens.sjogr@gmail.com>
2024-11-11 14:15:07 +0200

    add get_text function, make code more readable

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def main():
+BOOK_PATH = "books/frankenstein.txt"
 
-    with open ("books/frankenstein.txt") as f:
-        print(word_count(f.read()))
+def main():
+    text = get_text(BOOK_PATH)
+    count = word_count(text)
+    print(f"Word in book: {count}")
+
+def get_text(path):
+    with open (path) as f:
+        return f.read()
 
 def word_count(txt):
     word_count = len(txt.split())
